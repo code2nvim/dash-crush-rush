0fecd1a28a8b113177175fd23ebb43e65add0fc6
code2nvim <code2nvim@gmail.com>
2025-05-12 23:10:56 +0800

    init: dash-crush-rush

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::srgb_u8(23, 147, 209))) // #1793d1: Arch Linux
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "dash-crush-rush".into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
+}
