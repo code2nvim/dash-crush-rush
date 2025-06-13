9a6475474083d2b9cc162f747fdccff62db2b7f2
code2nvim <code2nvim@gmail.com>
2025-06-14 00:42:35 +0800

    make COLOR const

--- src/app/cfg.rs
+++ src/app/cfg.rs
@@ -6,12 +6,12 @@ pub mod camera {
 
 pub mod ground {
     pub const SIZE: (f32, f32) = (20.0, 20.0);
-    pub static COLOR: crate::Color = crate::Color::srgb(0.3, 0.5, 0.3);
+    pub const COLOR: crate::Color = crate::Color::srgb(0.3, 0.5, 0.3);
 }
 
 pub mod player {
     pub mod default {
         pub const RADIUS: f32 = 1.0;
-        pub static COLOR: crate::Color = crate::Color::srgb(0.5, 0.5, 0.5);
+        pub const COLOR: crate::Color = crate::Color::srgb(0.5, 0.5, 0.5);
     }
 }
