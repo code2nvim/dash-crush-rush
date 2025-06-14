383a8687491f0372eafd879d32452160bbdb612d
code2nvim <code2nvim@gmail.com>
2025-06-14 22:45:09 +0800

    use Single instead of Query in spawn_player

--- src/app/player.rs
+++ src/app/player.rs
@@ -22,21 +22,19 @@ pub fn spawn_player(
 pub fn move_player(
     key: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
-    mut transform: Query<&mut Transform, With<Player>>,
+    mut transform: Single<&mut Transform, With<Player>>,
 ) {
-    if let Ok(mut transform) = transform.single_mut() {
-        let distance = time.delta_secs() * default::SPEED;
-        if key.pressed(KeyCode::KeyW) {
-            transform.translation.z -= distance;
-        }
-        if key.pressed(KeyCode::KeyA) {
-            transform.translation.x -= distance;
-        }
-        if key.pressed(KeyCode::KeyS) {
-            transform.translation.z += distance;
-        }
-        if key.pressed(KeyCode::KeyD) {
-            transform.translation.x += distance;
-        }
-    };
+    let distance = time.delta_secs() * default::SPEED;
+    if key.pressed(KeyCode::KeyW) {
+        transform.translation.z -= distance;
+    }
+    if key.pressed(KeyCode::KeyA) {
+        transform.translation.x -= distance;
+    }
+    if key.pressed(KeyCode::KeyS) {
+        transform.translation.z += distance;
+    }
+    if key.pressed(KeyCode::KeyD) {
+        transform.translation.x += distance;
+    }
 }
